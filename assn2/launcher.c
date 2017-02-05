1dc9295fe2b1f8019b9f9d33b275d46656f21465
vagrant <vagrant@precise64.(none)>
2017-02-05 02:56:16 +0000

    don't compile things into themselves again

--- assn2/launcher.c
+++ assn2/launcher.c
@@ -1,31 +1,32 @@
 #include "assign2Headers.h"
 
-pid_t 	answererPid;
-pid_t 	guesserPid;
-int 	shouldRun	= 1;
+pid_t 			answererPid;
+pid_t 			guesserPid;
+int 	shouldRun 	= 1;
 
-void 	sigAlrmHandler (int sigInt)
+void sigAlrmHandler (int sigInt)
 {
 	kill(answererPid, TIME_OVER_SIGNAL);
 	kill(guesserPid, TIME_OVER_SIGNAL);
-	shouldRun 	= 0;
+	shouldRun = 0;
 }
 
-void 	sigChldHandler(int sigInt)
+void sigChldHandler(int sigInt)
 {
-	pid_t		pid;
-	int 		s;
+	pid_t	pid;
+	int s;
 	while((pid = waitpid(-1, &s, WNOHANG)) > 0) {}
-	shouldRun 	= 0;
+	shouldRun = 0;
 }
-	
 
 
-int 	main ()
+
+int main ()
 {
 	char			line[LINE_LEN];
 	struct 	sigaction	actC;
-	struct  sigaction       actA;	
+	struct  sigaction       actA;
+	int			status;
 
 	memset(&actC, '\0', sizeof(actC));
 	actC.sa_handler = sigChldHandler;
@@ -36,22 +37,31 @@ int 	main ()
         actA.sa_handler = sigAlrmHandler;
         actA.sa_flags = SA_NOCLDSTOP | SA_RESTART;
         sigaction(SIGALRM, &actA, NULL);
-	
+
 	pid_t childPid = fork();
-	printf("the pid for childPid is %d\n", getpid());	
-	
+	printf("the pid for childPid is %d\n", getpid());
+
 	if (childPid == 0) {
-		printf("the pid for the process when childPid = 0 is %d\n", getpid());
-//		exec("ANSWERER_PROGNAME",);
+		answererPid = getpid();
+		printf("the pid for the process when childPid = 0 is %d\n", answererPid);
+		exec("ANSWERER_PROGNAME","ANSWERER_PROGNAME", NULL);
+		fprintf(stderr, "Couldn not find %s\n", "ANSWERER_PROGNAME");
+		exit(EXIT_FAILURE);
 	}	
 	
-//	while (shouldRun)
-//		sleep(1);
-	
+	wait(&status);
+
+	if (WIFEXITED(status)) 	{
+		guesserPid = getpid();
+		printf("the pid for the process when childPid != 0 is %d\n", guesserPid);
+	}
+
+	//while (shouldRun)
+	//sleep(1);
+
 	sleep(1);
 	sleep(1);
-	
+
 	printf("launcher finished\n");
-	return(EXIT_SUCCESS);			
+	return(EXIT_SUCCESS);
 }
-    
