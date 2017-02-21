c4ffa94950055d1f2ca3b6ff4c27e2b4f9d14643
vagrant <vagrant@precise64.(none)>
2017-02-21 13:28:23 +0000

    building the linked list

--- assn3/separator.c
+++ assn3/separator.c
@@ -16,16 +16,19 @@ struct  		Word
 struct  Word*  		obtainCommaSeparatedList( const char* string) {
     const char 		comma 	 	= ',';
     char*  		charRun  	= strchr(string, comma); // pointer to next comma
-    int 		length 	 	= strlen(&string);
+    int 		length 	 	= strlen(string);
     struct  Word*	toReturn 	= (struct Word*)malloc(sizeof(struct Word));
-    int  		wordLen		= (charRun - &string + 1);
-    char 		wordText[wordLen];
+    size_t  		wordLen		= (charRun - string + 1);
+    char* 		wordText 	= (char*)malloc(wordLen);
+    
+    strncpy(wordText, string, wordLen);
         
-    toReturn -> text
+    toReturn->textPtr_ = wordText;
+    toReturn->nextPtr_ = obtainCommaSeparatedList( // the remainder of the string
     // want to allocate space from wordBeginning to charRun
     // want to use strncpy to get text from that length
     // make a Word object with (copiedText, charRun) as values
-    // 
+    // free things
 
     return(toReturn);
             
@@ -36,23 +39,26 @@ int main () {
     char*  	  	cPtr;
     char* 	 	charRun;
     struct  Word*  	splitList;
+    char* 		initChar;
+    char* 		commaChar;
 
     printf("Enter the comma-separated words to be delimited: ");
     fgets(line, MAX_LINE, stdin);
-    int 		initLen = strlen(line);
-    printf("strlen of this line is: %i \n", initLen);    
-    char*               noNewLine[initLen-1];
-    strncpy(noNewLine, line, initLen);
-//    printf("strncpy of this line is: %s", noNewLine);
-//    printf("|| no gap shows it worked");
 
-    cPtr = strchr(line,'\n');
-    if (cPtr != NULL) {
-         *cPtr = '\0';
-         splitList 	= obtainCommaSeparatedList(line);
-    } else {
-         printf("Invalid string entered; program terminating. \n");
-         printf("Enter something longer next time \n");
+    cPtr  	= strchr(line,'\n');
+    initChar  	= &line;
+    commaChar 	= strchr(line, ',');
+    printf("this is initChar: %s \n", initChar);
+    printf("this is the length between initChar and commaChar: %d \n", (commaChar - initChar));
+
+
+//    if (cPtr != NULL) {
+//         *cPtr = '\0';
+//         splitList 	= obtainCommaSeparatedList(line);
+//    } else {
+//         printf("Invalid string entered; program terminating. \n");
+//         printf("Enter something longer next time \n");
+
 //    printf("the entered sentence is %s \n", line);
 //    printf("the value in cPtr is %s \n", strchr(line, '\n'));
 
