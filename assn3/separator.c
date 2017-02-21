620431d16a3e2417c420b6a1c5345b9591ee26ed
vagrant <vagrant@precise64.(none)>
2017-02-21 00:19:45 +0000

    fleshing it out

--- assn3/separator.c
+++ assn3/separator.c
@@ -14,12 +14,20 @@ struct  		Word
 };
 
 struct  Word*  		obtainCommaSeparatedList( const char* string) {
-    char*  		charRun = strchr(string, ","); // pointer to next comma
-
+    const char 		comma 	 	= ',';
+    char*  		charRun  	= strchr(string, comma); // pointer to next comma
+    int 		length 	 	= strlen(&string);
+    struct  Word*	toReturn 	= (struct Word*)malloc(sizeof(struct Word));
+    int  		wordLen		= (charRun - &string + 1);
+    char 		wordText[wordLen];
+        
+    toReturn -> text
     // want to allocate space from wordBeginning to charRun
     // want to use strncpy to get text from that length
     // make a Word object with (copiedText, charRun) as values
+    // 
 
+    return(toReturn);
             
 }
 
@@ -27,7 +35,7 @@ int main () {
     char 	  	line[MAX_LINE];
     char*  	  	cPtr;
     char* 	 	charRun;
-    struct  Word*  	commaSepList = (struct Word*)malloc(sizeof(struct Word));
+    struct  Word*  	splitList;
 
     printf("Enter the comma-separated words to be delimited: ");
     fgets(line, MAX_LINE, stdin);
@@ -35,17 +43,18 @@ int main () {
     printf("strlen of this line is: %i \n", initLen);    
     char*               noNewLine[initLen-1];
     strncpy(noNewLine, line, initLen);
-    printf("strncpy of this line is: %s", noNewLine);
-    printf("|| no gap shows it worked");
+//    printf("strncpy of this line is: %s", noNewLine);
+//    printf("|| no gap shows it worked");
 
-    if (strlen(line) > 0) {
-        cPtr = strchr(line,'\n');
-        if (cPtr != NULL) 
-            *cPtr = '\0';
-    }
-
-    printf("the entered sentence is %s \n", line);
-    printf("the value in cPtr is %s \n", strchr(line, '\n'));
+    cPtr = strchr(line,'\n');
+    if (cPtr != NULL) {
+         *cPtr = '\0';
+         splitList 	= obtainCommaSeparatedList(line);
+    } else {
+         printf("Invalid string entered; program terminating. \n");
+         printf("Enter something longer next time \n");
+//    printf("the entered sentence is %s \n", line);
+//    printf("the value in cPtr is %s \n", strchr(line, '\n'));
 
     return(EXIT_SUCCESS);
 }
