5ebd6bf194b39c1303d93573903e0388f86adb45
vagrant <vagrant@precise64.(none)>
2017-02-22 02:19:47 +0000

    pre final cleanup

--- assn3/separator.c
+++ assn3/separator.c
@@ -14,8 +14,6 @@ struct  		Word
 };
 
 struct  Word*  		obtainCommaSeparatedList( const char* string) {
-    printf("just entered a new iteration, string = %s \n", string);
-    printf("string length = %d \n", strlen(string));
     char*  		charRun  	= strchr(string, ','); // pointer to next comma
     size_t 		wordLen;
     size_t              length          = strlen(string);
@@ -23,64 +21,55 @@ struct  Word*  		obtainCommaSeparatedList( const char* string) {
     char* 		stringRemnant;
 
     if (charRun != NULL) {
+        printf("charRun = %s \n", charRun);
         charRun  	+= 1;
-        printf("charRun is not NULL \n");
         wordLen 	= (charRun - string - 1);
         stringRemnant   = (char*)malloc(length - wordLen);
         strncpy(stringRemnant, charRun, (length - wordLen + 1));
     } else {
-        printf("charRun is NULL \n");
         wordLen  	= length;
         stringRemnant  	= string;
     }
-//    int 		length 	 	= strlen(string);
-    printf("right after setting length, length = %d \n", length);
-//    printf("right before setting wordLen: charRun = %s \n", charRun);
-    printf("banal stuff \n");
-//    size_t  		wordLen		= (charRun - string - 1);
-    char* 		wordText 	= (char*)malloc(wordLen+1);
-//    char* 		stringRemnant 	= (char*)malloc(length - wordLen);
-        
-    printf("debug: wordLen 		= %d \n", wordLen);
-    printf("debug: stringLength 	= %d \n", length); 
+
+    char* 		wordText 	= (char*)malloc(wordLen+1);       
     strncpy(wordText, string, wordLen);
-    printf("problem not in the wordText assignment \n");
-    printf("charRun = %s \n", charRun);
-//    strncpy(stringRemnant, charRun, (length - wordLen + 1));    
-    printf("Pre recn: length 	    = %d \n", length);
-    printf("Pre recn: wordLen       = %d \n", wordLen);    
-    printf("Pre recn: wordText      = %s \n", wordText);
-    printf("Pre recn: charRun       = %s \n", charRun);
-    printf("Pre recn: stringRemnant = %s \n", stringRemnant);  
     toReturn->textPtr_ = wordText;
 
-
     if (charRun == NULL) {
-//    if (endCond != NULL && charRun == NULL) {    
-//        toReturn->textPtr_ = wordText;
         printf("entering the recursion termination section");
         toReturn->nextPtr_ = NULL ;
-        free(stringRemnant);
         free(wordText);
-        free(toReturn);
+        free(stringRemnant);
         return(toReturn);
     } else {
         printf("in rec loop: stringRemnant = %s \n", stringRemnant);
         printf("in rec loop: remnantLength = %d \n", strlen(stringRemnant));
-        toReturn->nextPtr_ = obtainCommaSeparatedList(stringRemnant); // the remainder of the string
-        
+        toReturn->nextPtr_ = obtainCommaSeparatedList(stringRemnant); // the remainder of the string       
     }
         // want to allocate space from wordBeginning to charRun
         // want to use strncpy to get text from that length
         // make a Word object with (copiedText, charRun) as values
         // free things
         printf("Post recursion: wordText = %s \n", wordText);
-        free(stringRemnant);
-        free(wordText);
-        free(toReturn);
+    return(toReturn);           
+}
 
-    return(EXIT_FAILURE);
-            
+void 		printCommaSeparatedList(const struct Word* list) {
+    printf("Word text = \"%s\" \n", list->textPtr_);
+    if (list->nextPtr_ == NULL)
+        return;
+    printCommaSeparatedList(list->nextPtr_);
+}
+
+void 		freeCommaSeparatedList(struct Word* list) {
+    if (list->nextPtr_ == NULL) {
+        free(list->textPtr_);
+        return;
+    }
+    free(list->textPtr_);
+    freeCommaSeparatedList(list->nextPtr_);
+    printf("freed the node!\n");
+    free(list->nextPtr_);    
 }
 
 int main () {
@@ -98,19 +87,39 @@ int main () {
     initChar  	= &line;
     commaChar 	= strchr(line, ',');
     printf("this is initChar: %s \n", initChar);
-    printf("this is the length between initChar and commaChar: %d \n", (commaChar - initChar));
+    printf("this is the length between initChar and commaChar: %d \n",(int)(commaChar - initChar));
 
     printf("this is the remainder: %s \n", commaChar);
 
     if (cPtr != NULL) {
-         *cPtr = '\0';
-         splitList 	= obtainCommaSeparatedList(line);
+        *cPtr = '\0';
+        splitList 	= obtainCommaSeparatedList(line);
+        printCommaSeparatedList(splitList);
+        freeCommaSeparatedList(splitList);
     } else {
-         printf("Invalid string entered; program terminating. \n");
-         printf("Enter something longer next time \n");
+        printf("Invalid string entered; program terminating. \n");
+        printf("Enter something longer next time \n");
     }
-//    printf("the entered sentence is %s \n", line);
-//    printf("the value in cPtr is %s \n", strchr(line, '\n'));
+    printf("the entered sentence is %s \n", line);
+    printf("the value in cPtr is %s \n", strchr(line, '\n'));
+
+    struct  Word*       word1        = (struct Word*)malloc(sizeof(struct Word));
+    struct  Word*       word2        = (struct Word*)malloc(sizeof(struct Word));
+    struct  Word*       word3        = (struct Word*)malloc(sizeof(struct Word));
+    char*               wordText1    =(char*)malloc(4);
+    char*               wordText2    =(char*)malloc(4);
+    char*               wordText3    =(char*)malloc(4);
+    wordText1 = "word";
+    wordText2 = "bird";
+    wordText3 = "nerd";
+    word1->textPtr_ = wordText1;
+    word1->nextPtr_ = word2;
+    word2->textPtr_ = wordText2;
+    word2->nextPtr_ = word3;
+    word3->textPtr_ = wordText3;
+    word3->nextPtr_ = NULL;
+
+    printCommaSeparatedList(word1);
 
     return(EXIT_SUCCESS);
 }
