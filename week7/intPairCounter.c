4428bff46eb275740a02c00d563a793fa3c3b155
vagrant <vagrant@precise64.(none)>
2017-02-28 13:28:16 +0000

    more lecture code

--- week7/intPairCounter.c
+++ week7/intPairCounter.c
@@ -9,10 +9,57 @@ int 	main 	(int 	argc,
 		)
 {
     // Check the argument
-
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage:\tlineCounter <string> <filename>\n");
+        exit(EXIT_FAILURE);
+    }
     // Open the file
+    const char* lookFor = argv[1];
+    const char* filePath = argv[2];
+    FILE* filePtr = fopen(filePath, "r");
+    
+    if (filePtr == NULL)
+    {
+        fprintf(stderr, "Could not open %s\n", filePath);
+        exit(EXIT_FAILURE);
+    }
 
     // Read a lint
+    int 	counter = 0;
+    int 	lineNum = 0;
+    char 	buffer[BUFFER_LEN];
+
+    while (fgets(buffer, BUFFER_LEN, filePtr) != NULL)
+    {
+        if (strncmp(buffer, lookFor, lookForLen) == 0)
+            lineNum++;
+
+            char* 	cPtr;
+
+            for (cPtr = buffer;  isspace(*cPtr);  cPtr++);
+            if ( (*cPtr == COMMENT_CHAR || *cPtr == '\0') )
+                continue;
+
+            int i;
+            int j;
+
+            if ( sscanf(cPtr, "%d %d", &i, &j) == 2 )
+                counter++;
+            else
+                fprintf(stderr, "Missing integers on line %d\n", lineNum);
+    }
+
+    fclose(filePtr);
+    printf("%d\n", counter);
+    return(EXIT_SUCCESS);
+
+}
+
+
+    
+    
+
     //  go past the spaces
     //  in  see a COMMENT_CHAR or a blank line: continue
     //  try to read 2 integers
