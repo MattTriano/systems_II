1be60c4f2b4b4220244373ed578242987e161c82
vagrant <vagrant@precise64.(none)>
2017-03-07 00:17:35 +0000

    project files formated (maybe)

---
+++ assn4/mathServer.c
@@ -0,0 +1,171 @@
+/*-------------------------------------------------------------------------*
+ *---                                                                   ---*
+ *---                          mathServer.c                             ---*
+ *---                                                                   ---*
+ *---    This file defines a C program that gets file-sys commands      ---*
+ *---from client via a socket, executes those commands in their own     ---*
+ *---threads, and returns the corresponding output back to the          ---*
+ *---client.                                                            ---*
+ *---                                                                   ---*
+ *---  ----  ----  ---- ----  ----  ----  ----   ----                   ---*
+ *---                                                                   ---*
+ *---Version 1.02017 February 20Joseph Phillips                         ---*
+ *---                                                                   ---*
+ *-------------------------------------------------------------------------*/
+
+//Compile with:
+//$ gcc mathServer.c -o mathServer -lpthread
+
+//---Header file inclusion---//
+
+#include "mathClientServer.h"
+#include <errno.h> 		// For perror()
+#include <pthread.h> 		// For pthread_create()
+
+
+//---Definition of constants:---//
+
+#define		STD_OKAY_MSG		"Okay"
+
+#define		STD_ERROR_MSG		"Error doing operation"
+
+#define		STD_BYE_MSG		"Good bye!"
+
+#define		THIS_PROGRAM_NAME	"mathServer"
+
+#define		FILENAME_EXTENSION	".bc"
+
+#define		OUTPUT_FILENAME		"out.txt"
+
+#define		ERROR_FILENAME		"err.txt"
+
+#define		CALC_PROGNAME		"/usr/bin/bc"
+
+const int	ERROR_FD= -1;
+
+
+//---Definition of functions:---//
+
+
+
+//  PURPOSE:  To run the server by 'accept()'-ing client requests from
+//'listenFd' and doing them.
+void		doServer(int	listenFd
+			)
+{
+  //  I.  Application validity check:
+
+  //  II.  Server clients:
+  pthread_t		threadId;
+  pthread_attr_t	threadAttr;
+  int			threadCount= 0;
+
+  // YOUR CODE HERE
+}
+
+
+//  PURPOSE:  To decide a port number, either from the command line arguments
+//'argc' and 'argv[]', or by asking the user.  Returns port number.
+int		getPortNum(	int	argc,
+ 				char*	argv[]
+			  )
+{
+  //  I.  Application validity check:
+
+  //  II.  Get listening socket:
+  int	portNum;
+
+  if  (argc >= 2)
+    portNum= strtol(argv[1],NULL,0);
+  else
+  {
+    charbuffer[BUFFER_LEN];
+
+    printf("Port number to monopolize? ");
+    fgets(buffer,BUFFER_LEN,stdin);
+    portNum = strtol(buffer,NULL,0);
+  }
+
+  //  III.  Finished:  
+  return(portNum);
+}
+
+
+//  PURPOSE:  To attempt to create and return a file-descriptor for listening
+//to the OS telling this server when a client process has connect()-ed
+//to 'port'.  Returns that file-descriptor, or 'ERROR_FD' on failure.
+int		getServerFileDescriptor
+(intport
+)
+{
+  //  I.  Application validity check:
+
+  //  II.  Attempt to get socket file descriptor and bind it to 'port':
+  //  II.A.  Create a socket
+  int socketDescriptor = socket(AF_INET, // AF_INET domain
+        	SOCK_STREAM, // Reliable TCP
+	        0);
+
+  if  (socketDescriptor < 0)
+  {
+    perror(THIS_PROGRAM_NAME);
+    return(ERROR_FD);
+  }
+
+  //  II.B.  Attempt to bind 'socketDescriptor' to 'port':
+  //  II.B.1.  We'll fill in this datastruct
+  struct sockaddr_in socketInfo;
+
+  //  II.B.2.  Fill socketInfo with 0's
+  memset(&socketInfo,'\0',sizeof(socketInfo));
+
+  //  II.B.3.  Use TCP/IP:
+  socketInfo.sin_family = AF_INET;
+
+  //  II.B.4.  Tell port in network endian with htons()
+  socketInfo.sin_port = htons(port);
+
+  //  II.B.5.  Allow machine to connect to this service
+  socketInfo.sin_addr.s_addr = INADDR_ANY;
+
+  //  II.B.6.  Try to bind socket with port and other specifications
+  int status = bind(socketDescriptor, // from socket()
+    		   (struct sockaddr*)&socketInfo,
+     		   sizeof(socketInfo)
+    		   );
+
+  if  (status < 0)
+  {
+    perror(THIS_PROGRAM_NAME);
+    return(ERROR_FD);
+  }
+
+  //  II.B.6.  Set OS queue length:
+  listen(socketDescriptor,5);
+
+  //  III.  Finished:
+  return(socketDescriptor);
+}
+
+
+int  	main 	(int 	argc,
+  		 char*	argv[]
+	    	)
+{
+  //  I.  Application validity check:
+
+  //  II.  Do server:
+  int 	   port= getPortNum(argc,argv);
+  int      listenFd= getServerFileDescriptor(port);
+  int      status= EXIT_FAILURE;
+
+  if  (listenFd >= 0)
+  {
+    doServer(listenFd);
+    close(listenFd);
+    status= EXIT_SUCCESS;
+  }
+
+  //  III.  Finished:
+  return(status);
+}
